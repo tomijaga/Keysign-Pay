0f7c62a25b3c6016d3601125c853f64fa0ded6ea
Tomi Jaga <jagatomi12@gmail.com>
2021-11-05 15:10:39 -0400

    Delete react/ts/.history directory

--- react/ts/.history/src/KeysignPay_20211102160254.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const KeysignPay = () => {
-  return <div></div>;
-};
-
-export default KeysignPay;
--- react/ts/.history/src/KeysignPay_20211102165831.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const KeysignPay = () => {
-  return <div style={{ height: "80vh", backgroundColor: "white" }}></div>;
-};
-
-export default KeysignPay;
